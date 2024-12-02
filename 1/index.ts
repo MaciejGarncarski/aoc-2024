5167b2a5a1af3fb42c33fe548a85f76702762978
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-12-02 11:52:50 +0100

    day 1

---
+++ 1/index.ts
@@ -0,0 +1,28 @@
+import {rawInput} from "./input";
+
+const columns = rawInput.split("\n").map((el) => el.split('   '));
+
+const colA = []
+const colB = []
+
+columns.forEach((col) => {
+    colA.push(parseInt(col[0]))
+    colB.push(parseInt(col[1]))
+})
+
+const sortedColA = [...colA].sort();
+const sortedColB = [...colB].sort();
+
+const sum = sortedColA.reduce((acc, cur, idx) => {
+    const a = sortedColA[idx];
+    const b = sortedColB[idx];
+
+    if(a > b){
+        return acc + (a - b);
+    }
+
+    return acc + (b - a);
+}, 0);
+
+console.log(sum)
+// should be 3574690
