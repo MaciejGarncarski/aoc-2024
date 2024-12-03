a2551c8f3cc84b64a7143b55b505856a8dfbabcd
Maciej Garncarski <maciejgarncarski@protonmail.com>
2024-12-03 11:01:08 +0100

    add day 3, part 2

---
+++ 3/part-2.ts
@@ -0,0 +1,39 @@
+import {readInput} from "../utils/readInput.js";
+
+const input = readInput();
+
+const mulRegExp: RegExp = new RegExp('do\\(\\)|don\'t\\(\\)|\\bmul\\([1-9][0-9]{0,2},[1-9][0-9]{0,2}\\)', 'g');
+
+const matches = input.match(mulRegExp);
+
+type ReduceResult = {valid: boolean, muls: Array<number>};
+
+const mappedMatches = Array.from(matches || []).reduce((acc: ReduceResult, el) => {
+    if(el === "do()"){
+        return {...acc, valid: true};
+    }
+
+    if(el === "don't()"){
+        return {...acc, valid: false};
+    }
+
+    const numMuls = el
+        .replaceAll('mul(', '')
+        .replaceAll(',', ' ')
+        .replaceAll(')', '')
+        .split(' ')
+        .map((str) => parseInt(str, 10))
+        .reduce((acc, el) => acc * el, 1);
+
+    console.log(acc.valid)
+
+    const muls = acc.valid ? [...acc.muls, numMuls] : acc.muls
+
+    return {...acc, muls};
+}, {
+    muls: [],
+    valid: true,
+})
+
+const result = mappedMatches.muls.reduce((acc, el) => acc + el, 0);
+console.log(result, mappedMatches.muls)
